412f6353d0aa30142cbaa066f90565b82ebe39ff
DUVAN100 <duvanvivas100@gmail.com>
2026-06-05 19:33:26 -0500

    fix: remove test_connection.py

--- tests/unit/test_connection.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import asyncpg
-
-async def main():
-    conn = await asyncpg.connect("postgresql://postgres:SaludApiYa2026@db.bnqyyrwuiwivggwtpfmx.supabase.co:5432/postgres")
-    print("Conectado")
-    await conn.close()
-
-asyncio.run(main())
